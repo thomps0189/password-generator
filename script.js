f1d9d07ba59d253d8468121c00b901acfac7fe96
thomps0189 <angelathompson2011@gmail.com>
2021-10-24 21:41:19 -0500

    started generatePassword function

--- script.js
+++ script.js
@@ -1,28 +1,21 @@
 // Assignment code here
-// Arrays of all items
-const lowercaseArray = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"]
-
-const numbersArray = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
-
-//Prompt user to give ID Length
-function generatePW(x,y) {
-  let pwLength = parseInt(prompt('Length of the Password'))
-}
-
-if (isNaN(pwLength)) {
-  alert('Password Length must be a number')
-  return
-}
-
-if (pwLength < 8 || pwLength > 128) {
-  alert('Password must be between 8 and 128 characters')
-  return
-}
-
-//
 
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
+
+function generatePassword() {
+  console.log('you clicked the button')
+
+  // 1. Prompt the user for the password criteria. 
+  // Length 8 < 128; Lowercase, Uppercase, special characters
+  // 2. Confirm input is correct
+  // 3. Generate password
+
+
+  
+  // 4. Display password in box. 
+  return "Generated password will go here"
+}
 
 // Write password to the #password input
 function writePassword() {
