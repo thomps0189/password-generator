ae0b761a2ddb58dea9adc2a636eb7d5fb528a7e0
thomps0189 <angelathompson2011@gmail.com>
2021-10-24 20:53:09 -0500

    add arrays

--- script.js
+++ script.js
@@ -1,5 +1,25 @@
 // Assignment code here
+// Arrays of all items
+const lowercaseArray = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"]
 
+const numbersArray = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
+
+//Prompt user to give ID Length
+function generatePW(x,y) {
+  let pwLength = parseInt(prompt('Length of the Password'))
+}
+
+if (isNaN(pwLength)) {
+  alert('Password Length must be a number')
+  return
+}
+
+if (pwLength < 8 || pwLength > 128) {
+  alert('Password must be between 8 and 128 characters')
+  return
+}
+
+//
 
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
